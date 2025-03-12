ee0ebcf1257bfdb29778f73ebd3ba631546adc9a
elshencode13 <elshanss-ab206@code.edu.az>
2025-03-12 09:06:32 +0400

    Task-2 elave olundu

---
+++ MyFirstProject/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿#region Task-2
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 10, 15, 20, 25, };
+
+            int big = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[0] < array[i])
+                {
+                    big = array[i];
+
+                }
+            }
+            Console.WriteLine(big);
+        }
+    }
+}
+#endregion
