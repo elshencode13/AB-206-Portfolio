517d1fd6faf24dbfd39b65c64eb5077558a65bcd
elshencode13 <elshanss-ab206@code.edu.az>
2025-03-13 23:21:15 +0400

    Task2 yuklendi

---
+++ Task-2/Task-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, -4, 9, -8 };
+
+        Console.WriteLine( GetArray( numbers));
+        Console.WriteLine("Cem: "+ GetSum(numbers));
+        Console.WriteLine(Menfi(numbers));
+
+    }
+
+    
+}
---
+++ Task-2/Task-2/program2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class program2
+    {
+        public static void  GetArray (int[] array)
+         {
+            string netice = "";
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                netice += array[i];
+
+                Console.WriteLine( netice);
+
+            }
+        }
+        public static void GetSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                 sum += array[i];
+               
+            }
+            Console.WriteLine(sum);
+        }
+        public static bool Menfi(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
