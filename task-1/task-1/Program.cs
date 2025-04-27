a301b0a65f04972271e41124a900c9db24c27173
elshencode13 <elshanss-ab206@code.edu.az>
2025-04-27 14:10:43 +0400

    task1 yuklendi

---
+++ task-1/task-1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace task_1;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Age: ");
+                byte age = Convert.ToByte(Console.ReadLine());
+                Console.Write("Surname: ");
+                string surname = Console.ReadLine();
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+                Console.Write("Password: ");
+                int password = Convert.ToInt32(Console.ReadLine());
+
+                User user = new User(name, surname, age, username, password);
+                user.ShowFullData();
+                user.GetFullName();
+
+            
+        }
+    }
