a4122b6e3d028c6ffa501db3c8bcf73b47ad94a6
elshencode13 <elshanss-ab206@code.edu.az>
2025-04-27 14:16:05 +0400

    Task 2 yuklendi

---
+++ Task 2/Task 2/Notebook.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+using Task2;
+
+namespace Task2
+{
+    class Notebook : Product
+    {
+
+
+        public Notebook(string Name, int Count, int Price, byte RAM, int storage = 128, string Description = "Null") :
+        base(Name, Count, Price, Description)
+
+        {
+            _RAM = RAM;
+            Storage = storage;
+        }
+        private double _price;
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value > 0 && value < 10000)
+                {
+                    _price = Price;
+                }
+                else
+                {
+
+                    Console.WriteLine("Qiymeti duzgun daxil edin!");
+
+                }
+            }
+        }
+        public int Storage;
+
+        private byte _RAM;
+        public byte RAM
+        {
+            get { return _RAM; }
+            set
+            {
+                if (value > 0)
+                {
+                    _RAM = RAM;
+                }
+                else
+                {
+                    Console.WriteLine("Rami duzgun daxil edin.");
+                }
+            }
+        }
+
+
+        public string GetFullInfo()
+        {
+            return new string($"Name: {Name}\nCount: {Count}\nPrice: {Price}\nRAM: {RAM}\nStorage: {Storage}\nDescription: {Description}");
+        }
+        public void ShowFullData()
+        {
+            Console.WriteLine(GetFullInfo());
+        }
+
+        
+
+
+    }
+}
---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("Msi", 1, 2000,32);
+
+
+
+
+            notebook.GetFullInfo();
+            notebook.ShowFullData();
+        }
+    }
+}
