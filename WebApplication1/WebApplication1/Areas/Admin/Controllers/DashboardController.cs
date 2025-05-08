81d4bb907fef25eb93da1ed6ef5be8d2193fcef4
elshencode13 <elshanss-ab206@code.edu.az>
2025-05-08 11:55:06 +0400

    webapp

---
+++ WebApplication1/WebApplication1/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Contexts;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly AppDbContext _context;
+        public DashboardController()
+        {
+            _context = new AppDbContext();
+        }
+        //public DashboardController(AppDbContext context)
+        //{
+        //    _context = context;
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly string _connectionString = "Server=BAABAAA;Database=FiorelloDb;Trusted_Connection=True;TrustServerCertificate=True";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
